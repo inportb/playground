15262b974db52768badb19baa13be0a9d5c93f11
Lymia <lymiahugs@gmail.com>
2011-06-02 18:49:59 -0500

    Made more brutal.

--- Lymia/__init__.py
+++ Lymia/__init__.py
@@ -1,7 +1,7 @@
 def nuke():
 	import sys,imp,os,os.path
 	nukeList = ["Pwnna"]
-
+	debug = False
 	def nukeMain():
 		modules  = sys.modules
 		for moduleName in modules:
@@ -10,11 +10,13 @@ def nuke():
 				continue
 			for prefix in nukeList:
 				if moduleName.startswith(prefix):
+					if debug: print "Nuking "+moduleName
 					module.__dict__.clear()
 					break
 
 	def blockRecurse(path,modulename):
 		if os.path.exists(path):
+			if debug: print "Dummying "+modulename
 			sys.modules[modulename] = imp.new_module(modulename)
 			contents = os.listdir(path)
 
@@ -22,14 +24,11 @@ def nuke():
 				entryPath = path+"/"+entry
 				if entry.endswith(".py") and os.path.isfile(entryPath):
 					entryModulename = modulename+"."+entry[:len(entry)-3]
-					print "Dummying "+entryModulename
+					if debug: print "Dummying "+entryModulename
 					sys.modules[entryModulename] = imp.new_module(entryModulename)
 				elif os.path.isdir(entryPath):
-					entryModulename = modulename+"."+entry	
-					print "Dummying "+entryModulename
-					sys.modules[entryModulename] = imp.new_module(entryModulename)
-					print "Recursing into "+entryPath
-					blockRecurse(entryPath,entryModulename)
+					if debug: print "Recursing into "+entryPath
+					blockRecurse(entryPath,modulename+"."+entry)
 
 	nukeMain()
 	for prefix in nukeList:
