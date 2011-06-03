13e72e91197c23e522234b18ed41ec89cad532c0
Jiang Yio <inportb@gmail.com>
2011-06-02 23:54:30 -0400

    RPyC-gevent compatibility and monkey-patching code

--- Pinako/geventrpyc/__init__.py
+++ Pinako/geventrpyc/__init__.py
@@ -1,3 +1,30 @@
+## RPyC-gevent compatibility and monkey-patching code
+##
+## Copyright (C) 2011 by Jiang Yio <http://inportb.com/>
+##
+## Includes MIT-licensed code from RPyC:
+##   Copyright (c) 2005-2011
+##     Tomer Filiba (tomerfiliba@gmail.com)
+##     Copyrights of patches are held by their respective submitters
+##
+## Permission is hereby granted, free of charge, to any person obtaining a copy
+## of this software and associated documentation files (the "Software"), to deal
+## in the Software without restriction, including without limitation the rights
+## to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+## copies of the Software, and to permit persons to whom the Software is
+## furnished to do so, subject to the following conditions:
+##
+## The above copyright notice and this permission notice shall be included in
+## all copies or substantial portions of the Software.
+##
+## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+## IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+## FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+## AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+## LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+## OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+## THE SOFTWARE.
+
 from gevent import socket, spawn
 try:
 	from gevent import ssl
