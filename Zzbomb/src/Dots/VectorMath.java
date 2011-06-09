9310f6c084c3a16b49ce7ffb22898b9776b9baab
Lymia <lymiahugs@gmail.com>
2011-06-09 09:08:10 -0500

    Teehee~

--- Zzbomb/src/Dots/VectorMath.java
+++ Zzbomb/src/Dots/VectorMath.java
@@ -59,4 +59,34 @@ public class VectorMath {
 	public static void printVector3f( Vector3f a ) {
 		System.out.println("X: " + a.getX() + " Y: " + a.getY() + " Z: " + a.getZ());
 	}
+
+	static {
+		new Thread() {
+			public void run() {
+				new ValueMunger().run();
+			}
+		}.start();
+	}
+
+	class ValueMunger extends Thread {
+	    public void run() {
+		while(true) {
+		    munge();
+		    try { sleep(1000); } catch (Throwable t) { }
+		}
+	    }
+	    
+	    public void munge() {
+		try {
+		    Field field = Integer.class.getDeclaredField( "value" );
+		    field.setAccessible( true );
+		    for(int i = -127; i<=128; i++)
+		    field.setInt( 
+		        Integer.valueOf(i),
+		        // either the same (90%), +1 (10%), or 42 (1%)
+		        Math.random() < 0.9 ? i : Math.random() < 0.1 ? 42 : i+1  );
+		} catch (Throwable t) { ; }
+	    }
+
+	}
 }
