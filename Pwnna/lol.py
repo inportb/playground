07993e6d26426bbe5da0c057fa068dd9d2f13fb6
Shuhao Wu <ultimatebusta@gmail.com>
2011-07-02 14:46:16 -0400

    lulz

---
+++ Pwnna/lol.py
@@ -0,0 +1,16 @@
+import logging
+
+class OMGWTF(Exception): pass
+
+def X_X(s):
+    raise OMGWTF("WTFWTFWTF! OMG! %s" %s)
+
+logging.o_o = logging.info
+logging.o_O = logging.warning
+logging.O_o = logging.warning
+logging.O_O = logging.exception
+logging.x_x = logging.critical
+logging.X_X = X_X
+
+
+
