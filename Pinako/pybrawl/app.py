4c0af9484b3dee2fffc08229a88367866094b46d
Jiang Yio <inportb@gmail.com>
2011-07-04 17:36:18 -0400

    basic api

--- Pinako/pybrawl/app.py
+++ Pinako/pybrawl/app.py
@@ -9,11 +9,30 @@ from bottle import Bottle, static_file, redirect, request
 def Application():
 	app = Bottle()
 	app.catchall = False
+	# tasks
+	@app.route('/task',method='POST')
+	def post_task():
+		return {'result':False}
+	@app.route('/task/:uid',method='GET')
+	def get_task(uid):
+		return {}
+	# solutions
+	@app.route('/solution',method='POST')
+	def post_solution():
+		return {'result':False}
+	@app.route('/solution/:uid',method='GET')
+	def get_solution(uid):
+		return {}
+	# votes
+	@app.route('/vote',method='POST')
+	def post_vote():
+		return {'result':False}
+	# static files
 	@app.route('/')
-	def index():
+	def http_index():
 		return static_file('index.htm',root='./www')
 	@app.route('/:path#.+#')
-	def static(path):
+	def http_static(path):
 		return static_file(path,root='./www')
 	return app
 
